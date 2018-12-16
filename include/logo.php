ef79ade5f7d14477ed5f16a8ce02156b05d7e9ce
SultanMashukovH <mashukov.s@lema.agency>
2018-12-17 01:50:59 +0300

    Заменил логотип

--- include/logo.php
+++ include/logo.php
@@ -1,1 +1,2 @@
+<!--<img src="--><?//echo SITE_TEMPLATE_PATH?><!--/images/logo/logo.png" title="XShop" alt="XShop" />-->
 <img src="<?echo SITE_TEMPLATE_PATH?>/images/logo/logo.png" title="XShop" alt="XShop" />
